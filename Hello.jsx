be410adfd880c4696f7569481f491652ab252466
AaronRamales <99706675+ronnyyjay@users.noreply.github.com>
2024-01-05 15:14:00 +0800

    Add files via upload

---
+++ Hello.jsx
@@ -0,0 +1,10 @@
+export default function Hello() {
+    let message = "I think I've done it";
+    return(
+        <div>
+        <p>Hello Moon!</p>
+        {/* <h1>{message}</h1> */}
+        </div>
+    );
+
+    }
---
+++ Welcome.jsx
@@ -0,0 +1,10 @@
+export default function Welcome() {
+    let message = "I think I've done it";
+    return(
+        <div>
+        <p>Welcome my name is Aaron pogi!</p>
+        {<h1>{message}</h1> }
+        </div>
+    );
+
+    }
